1383ee888e3693fbeaa9bd69e87e80d6b5e405a4
Andre Braga <dnk_vip@hotmail.com>
2018-02-02 00:19:44 -0200

    Commit inicial estrutura da aplicacao

---
+++ www/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Revenda de Carros</title>
+        
+        <!--CDN Google-->
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+        <!-- CSS local-->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/nav.css">
+
+    </head>
+    <body>
+        <?php
+            /* Header Dinâmico */
+            include_once(__DIR__."/shared/header.php"); 
+        ?>
+
+        <?php
+            /* Body Dinâmico */
+            include_once(__DIR__."/shared/body.php"); 
+        ?>
+
+        <?php
+            /* Footer Dinâmico */
+            include_once(__DIR__."/shared/footer.php"); 
+        ?>
+
+        <!-- Script local-->
+        <script src="js/script.js"></script>
+    </body>
+</html>
---
+++ www/shared/nav.php
@@ -0,0 +1,8 @@
+<nav id="menu">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Catálogo</a></li>
+        <li><a href="#">Financiamento</a></li>
+        <li><a href="#">Contato</a></li>
+    </ul>
+</nav>
